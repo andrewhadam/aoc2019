49ee0ebc534a470f6ee47c1e61b4b4574eb907ad
Andrew Hadam <ahadam@athenahealth.com>
2019-12-01 14:37:47 -0500

    Day 1 solution and input values

--- day1/day1.go
+++ day1/day1.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 )
 
+func calcAllFuel(mass int) int {
+
+	var fuel = (mass / 3) - 2
+
+	if fuel > 0 {
+		return fuel + calcAllFuel(fuel)
+	} else {
+		return 0
+	}
+}
+
 func main() {
 
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var reqFuelPartOne = 0
+	var reqFuelPartTwo = 0
+
+	for scanner.Scan() {
+		toI, _ := strconv.Atoi(scanner.Text())
+		reqFuelPartOne += ((toI / 3) - 2)
+		reqFuelPartTwo += calcAllFuel(toI)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Part One Answer:")
+	fmt.Println(reqFuelPartOne)
+
+	fmt.Println("Part Two Answer:")
+	fmt.Println(reqFuelPartTwo)
 }
